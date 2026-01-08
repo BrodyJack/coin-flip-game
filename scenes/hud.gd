5fafb40d9ce0efeeda1d8c26b8ad118a6db7c795
Brody Johnstone <brodyjackj@gmail.com>
2026-01-07 22:04:30 -0500

    added upgrade functionality and additional sound effect

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -1,14 +1,48 @@
 extends CanvasLayer
 
 signal flip_coin
+signal coin_value_upgrade
+signal coin_chance_upgrade
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("spacebar_flip"):
 		flip_coin.emit()
+		
+func update_streak(streak) -> void:
+	$StreakLabel.text = "Streak: " + str(streak)
+	
+func update_total_cash(total_cash) -> void:
+	$CashAmountLabel.text = str(total_cash)
+
+func update_coin_value(coin_value) -> void:
+	$CoinValueUpgradeBox/PerFlipAmountLabel.text = str(coin_value)
+	
+func update_coin_value_upgrade_cost(upgrade_cost) -> void:
+	$CoinValueUpgradeBox/UpgradeCostAmountLabel.text = str(upgrade_cost)
+	
+func set_coin_value_upgrade_button_enabled(should_enable) -> void:
+	$CoinValueUpgradeBox/UpgradeButton.disabled = not should_enable
+
+func update_coin_chance(coin_chance) -> void:
+	$CoinChanceUpgradeBox/PerFlipChanceLabel.text = str(coin_chance) + "%"
+
+	
+func update_coin_chance_upgrade_cost(upgrade_cost) -> void:
+	$CoinChanceUpgradeBox/UpgradeCostAmountLabel.text = str(upgrade_cost)
+
+func set_coin_chance_upgrade_button_enabled(should_enable) -> void:
+	$CoinChanceUpgradeBox/UpgradeButton.disabled = not should_enable
+	
+func play_upgrade_sound() -> void:
+	$UpgradeButtonSound.play()
 
 func _on_flip_button_pressed() -> void:
 	flip_coin.emit()
 
 
-func update_streak(streak) -> void:
-	$StreakLabel.text = "Streak: " + str(streak)
+func _on_coin_value_upgrade_button_pressed() -> void:
+	coin_value_upgrade.emit()
+
+
+func _on_coin_chance_upgrade_button_pressed() -> void:
+	coin_chance_upgrade.emit()
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,13 +1,16 @@
 extends Node
 
-# Currently unused as we are instead simulating with physics
 # Consider reworking this to be the adjustable impulse, and to increase speed:
 # - Increase impulse + gravity
 # - Try -1500 w/ Gravity Scale 4.0 to test
+## **Currently unused as we are instead simulating with physics**
 @export var flip_time = 2.0
 
-# 
+## Chance for the coin to land on heads, 0 for tails, 1 for heads
 @export var heads_chance = .3
+
+## Cash granted on a heads flip
+@export var coin_value = 1.0
 
 const COIN_FRAME_HEADS = 0
 const COIN_FRAME_TAILS = 5
@@ -16,10 +19,19 @@ var is_coin_flipping = false
 var is_heads = false
 
 var streak = 0
+var total_cash = 0.0
+
+@export var coin_chance_upgrade_costs = [25, 50, 100, 200, 500]
+@export var coin_chance_upgrade_values = [.35, .40, .45, .50, .55]
+var coin_chance_upgrade_index = 0
+
+@export var coin_value_upgrade_costs = [10, 20, 40, 80, 160]
+@export var coin_value_upgrade_values = [2, 3, 4, 5, 10]
+var coin_value_upgrade_index = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	update_hud()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -48,12 +60,56 @@ func _on_coin_land(_body: Node) -> void:
 		$Coin/AnimatedSprite2D.frame = COIN_FRAME_HEADS
 		$Coin/HeadsSound.play()
 		streak += 1
+		total_cash += coin_value
 	else:
 		$Coin/AnimatedSprite2D.frame = COIN_FRAME_TAILS
 		$Coin/TailsSound.play()
 		streak = 0
 		
-	$HUD.update_streak(streak)
+	update_hud()
 	print("heads!" if is_heads else "tails")
 	is_coin_flipping = false
 	
+
+func update_hud() -> void:
+	$HUD.update_total_cash(total_cash)
+	$HUD.update_coin_chance(int(heads_chance * 100))
+	$HUD.update_coin_value(coin_value)
+	$HUD.update_streak(streak)
+	$HUD.update_coin_value_upgrade_cost(coin_value_upgrade_costs[coin_value_upgrade_index])
+	$HUD.update_coin_chance_upgrade_cost(coin_chance_upgrade_costs[coin_chance_upgrade_index])
+	
+	var should_enable = total_cash >= coin_value_upgrade_costs[coin_value_upgrade_index]
+	$HUD.set_coin_value_upgrade_button_enabled(should_enable)
+	
+	should_enable = total_cash >= coin_chance_upgrade_costs[coin_chance_upgrade_index]
+	$HUD.set_coin_chance_upgrade_button_enabled(should_enable)
+
+func _on_hud_coin_value_upgrade() -> void:
+	var current_upgrade_cost = coin_value_upgrade_costs[coin_value_upgrade_index]
+	var next_coin_value = coin_value_upgrade_values[coin_value_upgrade_index]
+	if total_cash < current_upgrade_cost:
+		return
+		
+	$HUD.play_upgrade_sound()
+	
+	total_cash -= current_upgrade_cost
+	coin_value = next_coin_value
+	coin_value_upgrade_index += 1
+	
+	update_hud()
+
+
+func _on_hud_coin_chance_upgrade() -> void:
+	var current_upgrade_cost = coin_chance_upgrade_costs[coin_chance_upgrade_index]
+	var next_coin_chance = coin_chance_upgrade_values[coin_chance_upgrade_index]
+	if total_cash < current_upgrade_cost:
+		return
+		
+	$HUD.play_upgrade_sound()
+	
+	total_cash -= current_upgrade_cost
+	heads_chance = next_coin_chance
+	coin_chance_upgrade_index += 1
+	
+	update_hud()
